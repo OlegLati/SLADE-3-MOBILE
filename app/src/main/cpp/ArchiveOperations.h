62e6fd2e6847426a9857b642a1ab0b2f31781e86
OlegLati <olegvivjasin.09@gmail.com>
2026-09-18 16:56:28 +0300

    refactor: extend archive operations for add and replace

--- app/src/main/cpp/ArchiveOperations.h
+++ app/src/main/cpp/ArchiveOperations.h
@@ -11,6 +11,8 @@ public:
     bool rename(ArchiveSession& session, unsigned index, const char* newName) const;
     bool remove(ArchiveSession& session, unsigned index) const;
     bool move(ArchiveSession& session, unsigned index, unsigned newPosition) const;
+    bool add(ArchiveSession& session, const char* name, const void* data, uint32_t size) const;
+    bool replace(ArchiveSession& session, unsigned index, const void* data, uint32_t size) const;
 };
 
 } // namespace slade_mobile
