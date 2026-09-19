11a1a63fd10f7603218cfddb814073440dbd92d2
OlegLati <olegvivjasin.09@gmail.com>
2026-09-19 08:36:19 +0300

    refactor: add native archive API boundary

---
+++ app/src/main/cpp/NativeArchiveApi.h
@@ -0,0 +1,46 @@
+#pragma once
+
+#include <cstdint>
+#include <string>
+
+#include "ArchiveSession.h"
+#include "ArchiveSerializer.h"
+#include "SaveCoordinator.h"
+#include "ArchiveOperations.h"
+#include "ArchiveEntryReader.h"
+
+namespace slade_mobile
+{
+
+class NativeArchiveApi
+{
+public:
+    bool open(const unsigned char* data, uint32_t size);
+    bool rename(unsigned index, const char* newName);
+    bool remove(unsigned index);
+    bool move(unsigned index, unsigned newPosition);
+
+    bool isDirty() const;
+
+    bool saveToFd(int fd);
+    const char* validateForSave();
+    bool commitSave(int fd);
+    bool discardChanges();
+
+    bool exportEntry(unsigned index, int fd);
+    bool addEntry(const char* name, int fd);
+    bool replaceEntry(unsigned index, int fd);
+
+    ArchiveSession& session();
+    const ArchiveSession& session() const;
+    ArchiveEntryReader& entryReader();
+
+private:
+    ArchiveSession session_;
+    ArchiveSerializer serializer_;
+    SaveCoordinator saveCoordinator_;
+    ArchiveOperations operations_;
+    ArchiveEntryReader entryReader_;
+};
+
+} // namespace slade_mobile
